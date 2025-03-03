eedee1c8d9eb85aca5c85095a590021146accb95
adedejiosvaldo <adedejiosvaldo@gmail.com>
2025-03-03 22:02:27 +0100

    Fix : Fixed dependency errir

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './entity/tasks.entity';
+import { TaskRepository } from './entity/task.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Task])],
   controllers: [TasksController],
-  providers: [TasksService]
+  providers: [
+    TasksService,
+    {
+      provide: 'TASK_REPOSITORY',
+      useClass: TaskRepository,
+    },
+  ],
 })
 export class TasksModule {}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { TaskRepository } from './entity/task.repository';
+import { CreateTaskDTO } from './dto/create-task.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(private readonly taskRepository: TaskRepository) {}
+
+  async createTask(body: CreateTaskDTO) {
+    try {
+      const task = await this.taskRepository.create(body);
+      return task;
+    } catch (error) {}
+  }
+}
