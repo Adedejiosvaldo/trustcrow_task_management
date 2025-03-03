9b4b254f03d8186bfa3c20923eca3e81d8053d8c
Joseph Adewunmi <adedejiosvaldo@gmail.com>
2025-03-03 13:24:31 +0100

    Feature : Added abstract repo interface

---
+++ src/common/database/abstract.interface.ts
@@ -0,0 +1,20 @@
+import {
+  DeepPartial,
+  DeleteResult,
+  FindManyOptions,
+  FindOneOptions,
+} from 'typeorm';
+
+export interface BaseInterfaceRepository<T> {
+  create(data: DeepPartial<T>): T;
+  save(data: DeepPartial<T>): Promise<T>;
+
+  findAll(options?: FindManyOptions<T>): Promise<T[]>;
+  findOneById(id: string): Promise<T>;
+  findOne(options: FindOneOptions<T>): Promise<T>;
+  findWithRelations(options: FindManyOptions<T>): Promise<T[]>;
+  findOneWithCondition(filterConditions: FindOneOptions<T>): Promise<T>;
+
+  update(id: string, data: Partial<T>): Promise<T | null>;
+  delete(id: string): Promise<DeleteResult>;
+}
