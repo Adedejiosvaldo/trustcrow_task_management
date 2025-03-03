684157e8769eb13b2de7b1706ddc050000b0253a
Joseph Adewunmi <adedejiosvaldo@gmail.com>
2025-03-03 12:19:51 +0100

    Chore (deps): Set up environment configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().default(5432),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        DB_SSL: Joi.boolean().default(false),
+        DB_CONNECTION_TIMEOUT: Joi.number().default(60000),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
