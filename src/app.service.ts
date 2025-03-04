060599c23459f78208d5b6184fe6fb5f91e6fab1
adedejiosvaldo <adedejiosvaldo@gmail.com>
2025-03-04 13:12:15 +0100

    Feature : Added cors support

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHealth(): { message: string } {
-    return { message: 'Server is up and running!' };
+  getHealth(): { message: string; documentation: string } {
+    return {
+      message: 'Server is up and running!',
+      documentation:
+        'https://documenter.getpostman.com/view/28957165/2sAYdkFnw9',
+    };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+  });
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
     new ValidationPipe({
